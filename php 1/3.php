1e073960cab10f592b6db793abdc36b6a362f7e1
Vladislav <vantyz757@gmail.com>
2025-02-16 01:16:28 +0300

    2

--- php 1/3.php
+++ php 1/3.php
@@ -1,9 +1,9 @@
 <?php
 $month = readline("Введите номер месяца от 1 до 12:");
 
-if ($month == 2) {
+if ($month === 2) {
     echo "Количество дней в месяце $month = 28";
-} elseif ($month == 4 || $month == 6 || $month == 9 || $month == 11) {
+} elseif ($month === 4 || $month === 6 || $month === 9 || $month === 11) {
     echo "Количество дней в месяце $month = 30";
 } else {
     echo "Количество дней в месяце $month = 31";
